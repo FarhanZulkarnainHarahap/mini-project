acf8f9a4053474bf555cca87b1d41f33216f4c80
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-05-29 21:52:22 +0700

    want to add create event

--- apps/web/src/components/organizerComponents/event-components/addTable.tsx
+++ apps/web/src/components/organizerComponents/event-components/addTable.tsx
@@ -212,31 +212,70 @@ export default function ShowAddNewEventSection() {
                   </div>
                 </div>
               )}
-              <div className="grid grid-rows-[auto_1fr] border">
-                <div>Reguler</div>
-                <div className="grid grid-rows-2">
-                  <div className="grid grid-cols-[auto_1fr] items-center border">
+              <div className="grid grid-rows-[auto_1fr] border py-5 rounded-2xl">
+                <div className="pl-2">
+                  <p>Regular</p>
+                </div>
+                <div className="grid grid-rows-2 gap-y-3">
+                  <div className="grid grid-cols-[auto_1fr] items-center ">
                     <div className="px-5 w-fit">
                       <p className="">Price :</p>
                     </div>
-                    <input className="border w-[50%]" value={e.target.value} />
+                    <input className="border w-[50%]" />
                   </div>
-                  <div className="grid grid-cols-[auto_1fr] items-center border">
+                  <div className="grid grid-cols-[auto_1fr] items-center ">
                     <div className="px-5 w-fit">
                       <p className="">Seat :</p>
                     </div>
-                    <input className="border lg:w-[100px] text-right" />
+                    <div className="pl-1">
+                      <input className="border lg:w-[100px] text-right" />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="grid grid-cols-2">
-                <div>VIP</div>
-                <input className="border" />
+
+              <div className="grid grid-rows-[auto_1fr] border py-5 rounded-2xl">
+                <div className="pl-2">
+                  <p>VIP</p>
+                </div>
+                <div className="grid grid-rows-2 gap-y-3">
+                  <div className="grid grid-cols-[auto_1fr] items-center">
+                    <div className="px-5 w-fit">
+                      <p className="">Price :</p>
+                    </div>
+
+                    <input className="border w-[50%]" />
+                  </div>
+                  <div className="grid grid-cols-[auto_1fr] items-center">
+                    <div className="px-5 w-fit">
+                      <p className="">Seat :</p>
+                    </div>
+                    <div className="pl-1">
+                      <input className="border lg:w-[100px] text-right" />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="grid grid-cols-2">
-                <div>VVIP</div>
-                <div>
-                  <input className="border" />
+
+              <div className="grid grid-rows-[auto_1fr] border py-5 rounded-2xl">
+                <div className="pl-2">
+                  <p>VVIP</p>
+                </div>
+                <div className="grid grid-rows-2 gap-y-3">
+                  <div className="grid grid-cols-[auto_1fr] items-center">
+                    <div className="px-5 w-fit">
+                      <p className="">Price :</p>
+                    </div>
+                    <input className="border w-[50%]" />
+                  </div>
+                  <div className="grid grid-cols-[auto_1fr] items-center ">
+                    <div className="px-5 w-fit">
+                      <p className="">Seat :</p>
+                    </div>
+                    <div className="pl-1">
+                      <input className="border lg:w-[100px] text-right" />
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="flex justify-end gap-3 pt-4">
@@ -266,7 +305,7 @@ export default function ShowAddNewEventSection() {
                   type="submit"
                   className="px-4 py-2 rounded bg-emerald-600 text-white hover:bg-emerald-700"
                 >
-                  Save
+                  Create
                 </button>
               </div>
             </form>
